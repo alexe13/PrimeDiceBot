1438afc7316aad9689ba5a30199d81dae88edcb0
alexe13 <darknessembraceme88@gmail.com>
2017-09-18 22:10:04 +0300

    changed to maven project

--- src/main/java/org/alex/Bet.java
+++ src/main/java/org/alex/Bet.java
@@ -1,4 +1,4 @@
-package src;
+package org.alex;
 
 import org.json.JSONObject;
 
--- src/main/java/org/alex/PrimeDice.java
+++ src/main/java/org/alex/PrimeDice.java
@@ -1,4 +1,4 @@
-package src;
+package org.alex;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.NameValuePair;
